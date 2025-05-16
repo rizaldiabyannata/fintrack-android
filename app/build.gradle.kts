c49f02806c54141c3181e5603e404de17bc9f2c3
Indi Ayuni <ayunilestari148@gmail.com>
2025-05-17 03:24:44 +0800

    fitur statistik

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -63,6 +63,8 @@ dependencies {
     implementation("com.google.android.material:material:1.12.0")
     implementation("com.google.android.gms:play-services-auth:21.0.0")
 
+    // Chart and graph library
+    implementation ("com.github.PhilJay:MPAndroidChart:v3.1.0")
 
 
 }
