fc5f3aae15a8e10c6714f25ac26cc5d71fb1ba2f
rizaldiabyannata <aldizar255@gmail.com>
2025-05-19 01:55:39 +0800

    Refactor: Update color values and apply rounded backgrounds
    - Changed primaryBlue and primaryRed color values in `colors.xml`.
    - Updated `fragment_tab_statistik.xml` to use new rounded drawables (`@drawable/rounded_blue`, `@drawable/rounded_red`) for percentage text backgrounds, adjusted padding, and text size.
    - Renamed `rounded_blue_button.xml` to `rounded_blue.xml`.
    - Added new drawable `rounded_red.xml` for red rounded backgrounds.
    - Minor adjustments to guidelines in `fragment_tambah_anggaran.xml`.
    - Removed trailing newline in `settings.gradle.kts`.
    - Added empty lines in `app/build.gradle.kts` for better readability.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -39,6 +39,8 @@ android {
     }
 }
 
+
+
 dependencies {
 
     implementation(libs.androidx.core.ktx)
@@ -64,7 +66,7 @@ dependencies {
     implementation("com.google.android.gms:play-services-auth:21.0.0")
 
     // Chart and graph library
-    implementation ("com.github.PhilJay:MPAndroidChart:v3.1.0")
+    implementation("com.github.PhilJay:MPAndroidChart:v3.1.0")
 
 
 }
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -22,4 +22,3 @@ dependencyResolutionManagement {
 
 rootProject.name = "Fintrack"
 include(":app")
- 
